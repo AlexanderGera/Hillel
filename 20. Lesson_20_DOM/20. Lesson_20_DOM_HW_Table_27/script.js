00e2e82611ef2b5599c82830d35ceb519b6163cc
“Alexander <gerasimenko.post@gmail.com>
2024-03-03 22:04:56 +0100

    HW 27

---
+++ 20. Lesson_20_DOM/20. Lesson_20_DOM_HW_Table_27/script.js
@@ -0,0 +1,26 @@
+'use strict'
+
+//ДЗ 27. table
+
+let tableWrapper = document.getElementsByClassName('table-wrapper')[0];
+
+let tableContainer = `<table>`;
+let cell = 0;
+let i;
+for (i = 1; i <= 10; i++) {
+   tableContainer += `<tr>`;
+   for(let k = 1; k <= 10; k++) {
+       cell++;
+       if (k < 10) {
+           tableContainer += `<td>${cell}</td>`;
+       }
+       if (k === 10) {
+           tableContainer += `<td>${cell}</td>` + `</tr>`;
+       }
+   }
+
+}
+tableContainer += `</table>`
+
+
+tableWrapper.innerHTML = tableContainer;
