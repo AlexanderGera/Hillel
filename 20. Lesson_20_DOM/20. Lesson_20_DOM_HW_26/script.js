42448704d2e38cfeeef46a0c9c44edc76e845e35
“Alexander <gerasimenko.post@gmail.com>
2024-03-03 17:18:09 +0100

    HW 26 with correction

--- 20. Lesson_20_DOM/20. Lesson_20_DOM_HW_26/script.js
+++ 20. Lesson_20_DOM/20. Lesson_20_DOM_HW_26/script.js
@@ -2,20 +2,17 @@
 
 //ДЗ 26. div - привид
 
-let myElement = document.getElementsByClassName('text-for-focus')[0];
+let myInput = document.getElementById('input_test');
 let myDiv = document.getElementsByClassName('main-holder__div')[0];
 
 
-myElement.addEventListener('mouseover', addFocus);
-myElement.addEventListener('mouseout', offFocus);
+myInput.addEventListener('focus', addFocus);
+myInput.addEventListener('blur', offFocus);
 
 function addFocus() {
-    myElement.classList.add('focus');
-    myDiv.classList.add('focus');
+    myDiv.classList.add('visible');
 }
 
-
 function offFocus() {
-    myElement.classList.remove('focus');
-    myDiv.classList.remove('focus');
+    myDiv.classList.remove('visible');
 }
