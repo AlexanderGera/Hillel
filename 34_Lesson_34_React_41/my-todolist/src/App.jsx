65275e54844eea0fc09bedc0ca9661296a65a244
“Alexander <gerasimenko.post@gmail.com>
2024-05-06 16:42:33 +0200

    HW 41 To-Do List: Envirement preparation

---
+++ 34_Lesson_34_React_41/my-todolist/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {MainContentWrapper} from "./components/MainContentWrapper/MainContentWrapper.jsx";
+import TodoList from "./components/TodoList/TodoList.jsx";
+
+
+
+function App() {
+    return (
+        <>
+
+
+
+        </>
+
+    )
+}
+
+export default App
